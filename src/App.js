002600ecc0770f644fabc63727c50e8050852370
Babur Shah Sayer <92582416+BaburshahSayer@users.noreply.github.com>
2023-01-10 08:55:08 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+//const Lists =['bob','john','mick'];
+const list = [
+  {id:1,title:"Assignment 1"},
+  {id:2,title:"Assignment 2"},
+  {id:3,title:"Assignment 3"},
+];
+
+function App() {
+    return (
+      <div>
+      <h1>TodoList</h1>
+      <ul>
+        {/* {list.map((index) => {
+                <li id={index.id}> {index.title} </li>
+                })} */}
+        {list.map(function(l){
+          return <li key={l.id}>{l.title}</li>
+        })}
+      </ul>
+      </div>
+    );
+}
+
+
+
+export default App;

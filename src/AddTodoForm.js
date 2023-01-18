5fe7762fb9d8c9459991a5c9294b7268660da719
Babur Shah Sayer <babursayer@gmail.com>
2023-01-18 21:06:33 +0430

    Add todo form component file

---
+++ src/AddTodoForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+let AddTodoForm =function(){
+
+    return (
+        <div>
+           <form>
+        <label htmlFor="todoTitle">
+            Title:
+        </label>
+            <input type="text" id="todoTitle" name="name" />
+
+        <button>Add</button>
+        </form>  
+        </div>
+    )
+
+}
+
+
+export default AddTodoForm;

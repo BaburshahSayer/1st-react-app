e52853a8c5705516bce24544bc0cad03d7b20265
Babur Shah Sayer <babursayer@gmail.com>
2023-02-07 23:01:42 +0430

    add needed changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import TodoList from "./TodoList"
+import AddTodoForm from "./AddTodoForm"
 
-function App(){
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <TodoList />
-            <AddTodoForm />
-        </div>
-    )
+
+function App() {
+  const [todoList, setTodoList] = useState([])
+  function addTodo(newTodo){
+    setTodoList([...todoList,newTodo])
+ }
+  return (
+    <>
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList}/>
+    </>
+  )
 }
-
-export default App;

24744642573f4fd81b6bce62ea4470ee16f59a00
Babur Shah Sayer <babursayer@gmail.com>
2023-02-07 23:02:18 +0430

    add todo list props

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,11 @@
-import React from "react";
+import React from "react"
+import TodoListItem from "./TodoListItem"
 
-const list = [
-  {id:1,title:"Assignment 1"},
-  {id:2,title:"Assignment 2"},
-  {id:3,title:"Assignment 3"},
-];
-let TodoList=function(){
-    return (
-        <div>
-            <h1>Hey CodeTheDream</h1>
-          <ul>    
-           {list.map(function(l){
-             return <li key={l.id}>{l.title}</li>
-           })}
-           </ul>
-        </div>
-    )
-}
+function TodoList(props) {
 
-export default TodoList; 
+  return (
+    <ul>
+      {props.todoList.map((todo) => (
+        <TodoListItem todo={todo} />
+      ))}
+    </ul>
